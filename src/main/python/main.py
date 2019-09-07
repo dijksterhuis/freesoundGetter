ec2e93aaad602eee70c29de9a3357b8b9ddea3b6
dijksterhuis-mb-macdesktop <mrobeson@dundee.ac.uk>
2019-09-07 22:00:29 +0100

    Modify the Query class to collect tags etc from user input, add inquirer as a dependency (although I might remove it)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -8,6 +8,7 @@ import sys
 import os
 import datetime
 import string
+import inquirer
 
 from secrets import CLIENT_ID, CLIENT_SECRET
 
@@ -48,11 +49,64 @@ class OAuth:
 class Query:
     def __init__(self, args):
         
-        self.filters = args.filters
         self.text = args.text
         self.fields = args.fields
         self.sort = args.sort
         self.group_packs = args.group_packs
+        
+        filetype, tags, duration_range, rating_range = self.__user_input()
+        self.filters = self.__build_filters(*self.__user_input())
+    
+    @staticmethod
+    def __build_filters(filetype, tags, duration_range, rating_range):
+        
+        filters = "type:({}) ".format(filetype)
+        filters += "tag:(" + " OR ".join(tags.split(" ")) + ") "
+        filters += " avg_rating:[{a} TO {b}]".format(
+            a=min(rating_range), 
+            b=max(rating_range)
+        )
+        filters += " duration:[{a} TO {b}]".format(
+            a=min(duration_range), 
+            b=max(duration_range)
+        )
+        
+        return filters
+        
+    @staticmethod
+    def __user_input():
+        
+        print("\n" + ("-" * 30) + "\nPlease seperate any query elements with a space delimiter...")
+        
+        filetype = None
+        tags = None
+        duration_range = None
+        rating_range = None
+        
+        while filetype not in {"wav", "mp3"}:
+            filetype = str(input("Filetype [wav/mp3]: ")).lower()
+        
+        while tags is None or tags is False:
+            tags = str(input("Tags: ")).lower()
+        
+        while duration_range is None:
+            duration_range = str(input("Duration lower and upper bounds: "))
+            try:
+                duration_range = {float(i) for i in duration_range.split(" ")}
+            except ValueError as e:
+                print("Error with input: {a}\n\nPlease try again...".format(a=e))
+                duration_range = None
+                
+        while rating_range is None:
+            rating_range = str(input("Duration lower and upper bounds: "))
+            try:
+                rating_range = {float(i) for i in rating_range.split(" ")}
+            except ValueError as e:
+                print("Error with input: {a}\n\nPlease try again...".format(a=e))
+                rating_range = None
+        
+        return filetype, tags, duration_range, rating_range
+        
 
 class ThrottlingException(Exception):
     pass
@@ -83,6 +137,7 @@ class Throttling:
         if self.count_daily == self.limit_daily: return ThrottlingException("Daily limit reached.")
         if self.count_hourly == self.limit_hourly: return ThrottlingException("Hourly limit reached.")
         if self.count_minute == self.limit_minute: return ThrottlingException("Minute limit reached.")
+        pass
     
     def __check_rates(self):
         pass
@@ -124,16 +179,24 @@ class FreesoundWrapper:
             )
         return self
     
-    def get_all(self, dl_dir):
+    def user_check(self):
         
         total_results = self.__results_pager.count
         page_count, sounds_count = (total_results // 15) + 1, 0
+        check = None
         
         check_string = "There are {a} total results to download...".format(a=total_results)
         check_string += "\nAre you sure you want to continue? [Y/N]: "
-        check = input(check_string)
         
-        if check == "N": return False
+        while check not in {"Y", "N"}:
+            check = str(input(check_string)).upper()
+        
+        if check == "N":
+            return False
+        else: 
+            return True
+    
+    def get_all(self, dl_dir):
         
         print(
             "{a} total results to get.\n{b} total pages.\nStarting download run...\n{c}\n".format(
@@ -144,9 +207,7 @@ class FreesoundWrapper:
         )
         
         for page_idx in range(page_count):
-            
             for sound in self.__results_pager:
-                
                 sys.stdout.write(
                     "\rGetting sound {a} of {b}... Sound ID: {c} Sound name: {d}{e}".format(
                         a = sounds_count, 
@@ -214,27 +275,6 @@ class Arguments:
             type = str
         )
         self.__args.add_argument(
-            "--tags", 
-            nargs = '+', 
-            type = str
-        )
-        self.__args.add_argument(
-            "--filetypes", 
-            nargs = '+',
-            default = "wav",
-            type = str
-        )
-        self.__args.add_argument(
-            "--duration_range", 
-            nargs = 2, 
-            type = float
-        )
-        self.__args.add_argument(
-            "--rating_range", 
-            nargs = 2, 
-            type = float
-        )
-        self.__args.add_argument(
             "--sort", 
             nargs = 1, 
             choices = ["rating_desc", "rating_asc", "downloads_desc", "downloads_asc", "duration_desc", "duration_asc"], 
@@ -254,27 +294,7 @@ class Arguments:
         
         args = self.__args.parse_args()
         
-        if args.filetypes and len(args.filetypes) > 1:
-            filters = "type:(" + " OR ".join(args.filetypes) + ") "
-        elif args.filetypes:
-            filters = "type:{} ".format(args.filetypes[0])
-        else:
-            filters = ""
-        
-        filters += "tag:(" + " OR ".join(args.tags) + ") " if args.tags else ""
-        
-        filters += " avg_rating:[{a} TO {b}]".format(
-            a=min(args.rating_range), 
-            b=max(args.rating_range)
-        ) if args.rating_range else ""
-        
-        filters += " duration:[{a} TO {b}]".format(
-            a=min(args.duration_range), 
-            b=max(args.duration_range)
-        ) if args.duration_range else ""
-        
         self.text = args.text_query if args.text_query else None
-        self.filters = filters if filters != "" else None
         self.fields = ",".join(args.fields)
         self.sort = args.sort
         self.group_packs = args.group_packs
@@ -283,10 +303,13 @@ class Arguments:
 
 def main():
     args = Arguments()
-    query = Query(args.build())
     auth = OAuth(CLIENT_ID, CLIENT_SECRET)
     fs = FreesoundWrapper(auth.oauth())
-    fs.search(query).get_all(os.path.join(os.getenv("HOME"), "Downloads/freesound/"))
+    result = False
+    while result is False:
+        query = Query(args.build())
+        result = fs.search(query).user_check()
+    fs.get_all(os.path.join(os.getenv("HOME"), "Downloads/freesound/"))
     
 if __name__ == '__main__':
     main()
